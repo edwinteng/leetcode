54321612ac56ce88669f1458cd6023ce06d2a20a
edwinteng <edwinteng@gmail.com>
2024-11-18 14:43:44 -0500

    Time: 95 ms (5.06%) | Memory: 27.6 MB (21.59%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        ptra = headA
+        ptrb = headB
+        #while ptra or ptrb:
+        while ptra != ptrb:
+            #if ptra and ptrb and ptra.val == ptrb.val:
+            #    return ptra.val
+            if ptra is None:
+                ptra = headB
+            else:
+                ptra = ptra.next
+            if ptrb is None:
+                ptrb = headA
+            else:
+                ptrb = ptrb.next
+            #ptra = ptra.next
+            #ptrb = ptrb.next
+        return ptra
