4999115ba6f6dac42de8d251f473bbe0da2c8d1c
edwinteng <edwinteng@gmail.com>
2024-11-18 14:45:59 -0500

    Time: 91 ms (10.11%) | Memory: 27.5 MB (25.43%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -12,14 +12,16 @@ class Solution:
         while ptra != ptrb:
             #if ptra and ptrb and ptra.val == ptrb.val:
             #    return ptra.val
-            if ptra is None:
-                ptra = headB
-            else:
-                ptra = ptra.next
-            if ptrb is None:
-                ptrb = headA
-            else:
-                ptrb = ptrb.next
+            #if ptra is None:
+            #    ptra = headB
+            #else:
+            #    ptra = ptra.next
+            #if ptrb is None:
+            #    ptrb = headA
+            #else:
+            #    ptrb = ptrb.next
             #ptra = ptra.next
             #ptrb = ptrb.next
+            ptra = ptra.next if ptra else headB
+            ptrb = ptrb.next if ptrb else headA
         return ptra
