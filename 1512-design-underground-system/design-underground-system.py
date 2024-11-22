d24dd8a3b4ece7e45b3369af1d77f68f5a1acf5e
edwinteng <edwinteng@gmail.com>
2024-11-21 23:45:12 -0500

    Time: 41 ms (7.55%) | Memory: 28.2 MB (37.18%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -9,15 +9,19 @@ class UndergroundSystem:
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         stationPrev,tPrev = self.checkin[id]
-        if (stationPrev,stationName) not in self.travel_time:
-            self.travel_time[(stationPrev,stationName)] = [t-tPrev]
-        else:
-            self.travel_time[(stationPrev,stationName)].append(t-tPrev)
+        #if (stationPrev,stationName) not in self.travel_time:
+        #    self.travel_time[(stationPrev,stationName)] = [t-tPrev]
+        #else:
+        #    self.travel_time[(stationPrev,stationName)].append(t-tPrev)
+        traveltime,numTrip = self.travel_time.get((stationPrev,stationName),(0,0))
+        self.travel_time[(stationPrev,stationName)]=(traveltime+t-tPrev,numTrip+1)
         del self.checkin[id]
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        timeList = self.travel_time[(startStation,endStation)]
-        return sum(timeList)/len(timeList)
+        totaltime,numTrip = self.travel_time[(startStation,endStation)]
+        return totaltime/numTrip
+        #timeList = self.travel_time[(startStation,endStation)]
+        #return sum(timeList)/len(timeList)
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
