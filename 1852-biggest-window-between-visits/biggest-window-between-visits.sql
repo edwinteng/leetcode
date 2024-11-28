613d1971fce9de42ed348d9481e58f07ae301e6d
edwinteng <edwinteng@gmail.com>
2024-11-27 22:36:02 -0500

    Time: 2999 ms (5.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+WITH t as (
+SELECT
+user_id,
+visit_date,
+IFNULL(datediff(lead(visit_date,1) over (partition by user_id order by visit_date asc),visit_date),datediff('2021-01-01',visit_date)) as date_diff
+FROM UserVisits
+)
+SELECT user_id, max(date_diff) as biggest_window
+FROM t 
+group by user_id
+HAVING max(date_diff) is not null
