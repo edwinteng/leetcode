5a90e5c86a73e2dba32e693d39bba1468479937d
edwinteng <edwinteng@gmail.com>
2024-12-01 21:49:16 -0500

    Time: 3 ms (58.83%) | Memory: 12.4 MB (5.74%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -8,25 +8,18 @@ class Solution(object):
         cur_len= 0
         fast,slow = 0,0
         while fast < len(chars):
-            if chars[slow] == chars[fast]:
+
+            while fast < len(chars) and chars[slow] == chars[fast]:
                 fast+=1
-            else:
-                length = fast-slow
-                chars[cur_len] = chars[slow]
-                cur_len+=1
-                if length >1:
-                    #convert number to string
-                    int_str = str(length)
-                    for i in range(len(int_str)):
-                        chars[cur_len+i] = int_str[i]
-                    cur_len+=len(int_str)
-                slow = fast
-        length=fast-slow
-        chars[cur_len]= chars[slow]
-        cur_len+=1
-        if length > 1:
-            int_str = str(length)
-            for i in range(len(int_str)):
-                chars[cur_len+i]=int_str[i]
-            cur_len+=len(int_str)
+                
+            length = fast-slow
+            chars[cur_len] = chars[slow]
+            cur_len+=1
+            if length >1:
+                #convert number to string
+                int_str = str(length)
+                for i in range(len(int_str)):
+                    chars[cur_len+i] = int_str[i]
+                cur_len+=len(int_str)
+            slow = fast
         return cur_len
