7fb0d65a52fea688100aeb3bc0a3112793a98adb
edwinteng <edwinteng@gmail.com>
2024-12-11 23:23:28 -0500

    Time: 63 ms (55.80%) | Memory: 31.6 MB (16.59%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,10 +4,9 @@ class Solution:
         max_s = nums[0]
         s = nums[0]
         for i in range(1,len(nums)):
-            #start new or add new
-            
             if s <0:
                 s = 0
             s+=nums[i]
-            max_s = max(max_s,s)
-        return max_s 
+            max_s=max(max_s,s)
+        return max_s
+            
