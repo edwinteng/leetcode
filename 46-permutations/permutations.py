fe4edd550995e9070eae675174ddf851d2efc1f1
edwinteng <edwinteng@gmail.com>
2024-12-13 19:45:29 -0500

    Time: 3 ms (36.31%) | Memory: 17.7 MB (8.81%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        mask = [False]*len(nums)
+        ans = []
+        def dfs(index,path):
+            if index == len(nums):
+                ans.append(path)
+                return
+            for i in range(len(nums)):
+                if not mask[i]:
+                    mask[i] = True
+                    dfs(index+1,path+[nums[i]])
+                    mask[i] = False
+        dfs(0,[])
+        return ans
+
