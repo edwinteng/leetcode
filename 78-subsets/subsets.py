db2ad420c9fa6960eb902af948179624c39bf490
edwinteng <edwinteng@gmail.com>
2024-12-13 23:19:45 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (11.17%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,13 +2,8 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
         def dfs(index,path):
-            if index == len(nums):
-                #print('add '+str(index))
-                ans.append(path)
-                return
-            #print(str(index)+':'+str(len(nums)))
-
-            dfs(index+1,path+[nums[index]])
-            dfs(index+1,path)
+            ans.append(path)
+            for i in range(index,len(nums)):
+                dfs(i+1,path+[nums[i]])
         dfs(0,[])
         return ans
