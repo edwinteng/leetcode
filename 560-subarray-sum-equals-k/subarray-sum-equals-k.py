5e6e4d38595073f8ef58c5a0594b0ae121d3346a
edwinteng <edwinteng@gmail.com>
2024-12-16 23:33:23 -0500

    Time: 39 ms (50.43%) | Memory: 14.3 MB (52.29%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,17 @@
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        #get prefix sum
-        d={0:1}
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        d = {0:1}
+        count=0
         cum_sum = 0
-        count =0
-        for i in range(len(nums)):
-            cum_sum+=nums[i]
-            
+        for n in nums:
+            cum_sum+=n
             if cum_sum-k in d:
                 count+=d[cum_sum-k]
-            d[cum_sum]=d.get(cum_sum,0)+1
+            d[cum_sum] = d.get(cum_sum,0)+1
         return count
+
