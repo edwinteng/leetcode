663d37c62715adf9f8f38650650e701ec440da7e
edwinteng <edwinteng@gmail.com>
2024-12-21 20:36:39 -0500

    Time: 4 ms (10.82%) | Memory: 16.5 MB (8.32%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        def dfs(root):
+            if not root:
+                return None
+            if root.val==val:
+                return root
+            return dfs(root.left) or dfs(root.right)
+        return dfs(root)
