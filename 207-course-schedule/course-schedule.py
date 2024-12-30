f33ff85c0c7581bfce5536327be5c8f93c82df28
edwinteng <edwinteng@gmail.com>
2024-12-30 15:10:40 -0500

    Time: 7 ms (49.80%) | Memory: 19 MB (26.80%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,15 +3,15 @@ class Solution:
         indegree = {i:0 for i in range(numCourses)}
         adj = {i:[] for i in range(numCourses)}
         for course,pre in prerequisites:
-            adj[pre].append(course)
             indegree[course]+=1
+            adj[pre].append(course)
         q = deque([i for i in range(numCourses) if indegree[i]==0])
-        num_course = 0
+        course_seq = []
         while q:
             course = q.popleft()
-            num_course+=1
+            course_seq.append(course)
             for next_course in adj[course]:
                 indegree[next_course]-=1
                 if indegree[next_course]==0:
                     q.append(next_course)
-        return num_course == numCourses
+        return numCourses == len(course_seq)
