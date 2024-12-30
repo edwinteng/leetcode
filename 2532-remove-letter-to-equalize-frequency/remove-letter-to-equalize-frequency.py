be8345c2a867d4bb05d27df79e3c501112603a81
edwinteng <edwinteng@gmail.com>
2024-12-29 22:21:27 -0500

    Time: 1 ms (23.76%) | Memory: 17.8 MB (18.32%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def equalFrequency(self, word):
-        """
-        :type word: str
-        :rtype: bool
-        """
-        d_freq = Counter(word)
-        
-        for key in d_freq.keys():
-            d_freq[key]-=1
-            s = set([val for val in d_freq.values() if val>0])
-            if len(s) ==1:
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        d = Counter(word)
+        for char in word:
+            d[char]-=1
+            s = [val for val in d.values() if val>0]
+            if len(set(s))==1:
                 return True
-            d_freq[key]+=1
+            d[char]+=1
         return False
