cd55662b95628a09ada8092c1df62d22fd4ace5d
edwinteng <edwinteng@gmail.com>
2024-12-29 21:16:07 -0500

    Time: 262 ms (35.70%) | Memory: 25.4 MB (16.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,24 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_row = len(grid)
-        num_col = len(grid[0])
-        def get_neighbor(coord):
-            delta_r = [1,-1,0,0]
-            delta_c = [0,0,1,-1]
-            r,c = coord
-            res = list()
-            for i in range(len(delta_r)):
-                neighbor_r = r+delta_r[i]
-                neighbor_c = c+delta_c[i]
-                if 0<=neighbor_r<num_row and 0<=neighbor_c<num_col:
-                    res.append((neighbor_r,neighbor_c))
-            return res
-        def bfs(start):
-            queue = deque([start])
-            while len(queue)>0:
-                coord = queue.popleft()
-                for neighbor in get_neighbor(coord):
-                    r,c = neighbor
-                    if grid[r][c] == '1':
-                        queue.append(neighbor)
-                        grid[r][c] = '0'
+        num_row,num_col = len(grid),len(grid[0])
+        visited = set()
+        
+        def bfs(x,y):
+            q = deque([(x,y)])
+            visited.add((x,y))
+            while q:
+                cur_i,cur_j = q.popleft()
+                for d_i,d_j in [(1,0),(0,1),(-1,0),(0,-1)]:
+                    i,j = cur_i+d_i,cur_j+d_j
+                    if 0<=i < num_row and 0<=j<num_col and grid[i][j]=='1' and (i,j) not in visited:
+                        q.append((i,j))
+                        visited.add((i,j))
         num_island = 0
-        for  i in range(num_row):
+        for i in range(num_row):
             for j in range(num_col):
-                if grid[i][j] == '1':
-                    bfs((i,j))
+                if grid[i][j]=='1' and (i,j) not in visited:
+                    bfs(i,j)
                     num_island+=1
         return num_island
 
