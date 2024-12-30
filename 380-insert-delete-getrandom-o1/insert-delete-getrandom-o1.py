4be90f2d65529f7ea261bd5c9ada7d81729067d8
edwinteng <edwinteng@gmail.com>
2024-12-29 22:30:45 -0500

    Time: 121 ms (64.24%) | Memory: 57.8 MB (9.90%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,6 +1,8 @@
-import random
-class RandomizedSet:
 
+class RandomizedSet:
+    # O(1) for insert:  any data structure
+    # hash for O(1)
+    # random list   random.choice()
     def __init__(self):
         self.hash = {}
         self.lst = []
@@ -9,26 +11,25 @@ class RandomizedSet:
         if val in self.hash:
             return False
         else:
-            index = len(self.lst)
-            self.hash[val] = index
+            self.hash[val]= len(self.lst)
             self.lst.append(val)
-            #print('add:'+str(self.lst))
             return True
     def remove(self, val: int) -> bool:
         if val in self.hash:
-            index = self.hash[val]
-            #last_index = self.hash[self.lst[-1]]
-            self.hash[self.lst[-1]]=index
-            self.lst[index],self.lst[len(self.lst)-1]=self.lst[len(self.lst)-1],self.lst[index]
+
+            index,last_index = self.hash[val],len(self.lst)-1
+            last_val = self.lst[-1]
+            self.hash[last_val] = index
+            self.lst[index],self.lst[last_index]=self.lst[last_index],self.lst[index]
             self.lst.pop()
             del self.hash[val]
-            #print('remove:'+str(self.lst))
             return True
         else:
             return False
 
     def getRandom(self) -> int:
         return random.choice(self.lst)
+
 
 
 # Your RandomizedSet object will be instantiated and called as such:
