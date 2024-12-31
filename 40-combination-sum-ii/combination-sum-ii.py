65e6ca98048e6997bc25ec6f93b3348cc41b95ad
edwinteng <edwinteng@gmail.com>
2024-12-31 13:26:57 -0500

    Time: 26 ms (39.05%) | Memory: 17.8 MB (11.67%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,31 +1,16 @@
-class Solution(object):
-    def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         ans = []
         def dfs(index,total,path):
-            if total>target or index>len(candidates):
-                return
-            #while index-1>=0 and candidates[index]==candidates[index-1]:
-            #    index+=1
             if total==target:
                 ans.append(path)
                 return
-            #1 1 2 5 6 7
-            #0 0 
-
+            if index == len(candidates) or total>target:
+                return
             for i in range(index,len(candidates)):
-                if i!=index and candidates[i]==candidates[i-1]:
+                if index!=i and candidates[i]==candidates[i-1]:
                     continue
                 dfs(i+1,total+candidates[i],path+[candidates[i]])
-                    #continue
-                #if index <len(candidates) and total<target:
-                #dfs(i+1,total+candidates[i],path+[candidates[i]])
-                #dfs(index+1,total,path)
         dfs(0,0,[])
         return ans
-        
