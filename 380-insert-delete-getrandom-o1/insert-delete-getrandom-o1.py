2f1cd898184bb0b9d7794235fb218537f2175e00
edwinteng <edwinteng@gmail.com>
2025-01-02 17:12:15 -0500

    Time: 92 ms (93.51%) | Memory: 58 MB (7.78%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -8,28 +8,30 @@ class RandomizedSet:
         self.lst = []
 
     def insert(self, val: int) -> bool:
-        if val in self.hash:
-            return False
-        else:
-            self.hash[val]= len(self.lst)
+        if val not in self.hash:
+            self.hash[val] = len(self.lst)
             self.lst.append(val)
             return True
-    def remove(self, val: int) -> bool:
-        if val in self.hash:
-
-            index,last_index = self.hash[val],len(self.lst)-1
-            last_val = self.lst[-1]
-            self.hash[last_val] = index
-            self.lst[index],self.lst[last_index]=self.lst[last_index],self.lst[index]
-            self.lst.pop()
-            del self.hash[val]
-            return True
         else:
+
             return False
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hash:
+            return False
+        else:
+            index = self.hash[val]
+            val_last = self.lst[-1]
+            self.hash[val_last] = index
+            self.lst[index],self.lst[-1]=self.lst[-1],self.lst[index]
+
+            del self.hash[val]
+            self.lst.pop()
+            return True
+
 
     def getRandom(self) -> int:
         return random.choice(self.lst)
-
 
 
 # Your RandomizedSet object will be instantiated and called as such:
