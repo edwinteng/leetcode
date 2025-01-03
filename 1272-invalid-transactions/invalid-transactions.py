7e6c2610892a7585b7e0e3197485a57af844cab4
edwinteng <edwinteng@gmail.com>
2025-01-02 22:01:16 -0500

    Time: 64 ms (26.52%) | Memory: 18.5 MB (10.30%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,21 +1,21 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        ans = set()
-        d = {}
-        anslist = list()
-        for i in range(len(transactions)):
-            name,time_str,amount_str,city = transactions[i].split(',')
-            time,amount = int(time_str),int(amount_str)
-            if amount>1000:
-                ans.add(i)
-            if name not in d:
-                d[name]=list() 
-            d[name].append([int(time),city,i])
-            #print(d[name])
-            for othertime,othercity,otheri in d[name]:
-                if otheri!=i and abs(time-othertime)<=60 and city!=othercity:
-                    ans.add(otheri) 
-                    ans.add(i)
-
-
-        return [transactions[i] for i in ans]
+        #same transaction can have different index/seq but can have same value
+        idx = set()
+        d = defaultdict(list)
+        #loop through transaction
+        for i, tran in enumerate(transactions):
+            name,time,amount,city = tran.split(',')
+            d[name].append((time,amount,city,i))
+        #check if amount > 1000
+            if int(amount)>1000:
+                idx.add(i)
+        # add transaction to answer list if not added before
+            for cur_time,cur_amount,cur_city,cur_i in d[name]:
+        #compare with transactions associated with same name ->  dictionary
+                if cur_city!=city and abs(int(cur_time)-int(time))<=60:
+        # add transactions to answer list if not added before
+                    idx.add(cur_i)
+                    idx.add(i)
+        # return transaction
+        return [transactions[i] for i in idx]
