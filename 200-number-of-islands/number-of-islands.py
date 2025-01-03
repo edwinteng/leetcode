6eb61dd377bf30b3bdc63554247edde1a6af029b
edwinteng <edwinteng@gmail.com>
2025-01-02 21:06:28 -0500

    Time: 279 ms (22.87%) | Memory: 25 MB (21.28%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,24 +3,26 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         num_row,num_col = len(grid),len(grid[0])
         visited = set()
-        
-        def bfs(x,y):
-            q = deque([(x,y)])
-            visited.add((x,y))
+        def bfs(i,j):
+            q = deque()
+            q.append((i,j))
+            visited.add((i,j))
             while q:
                 cur_i,cur_j = q.popleft()
-                for d_i,d_j in [(1,0),(0,1),(-1,0),(0,-1)]:
-                    i,j = cur_i+d_i,cur_j+d_j
-                    if 0<=i < num_row and 0<=j<num_col and grid[i][j]=='1' and (i,j) not in visited:
-                        q.append((i,j))
-                        visited.add((i,j))
-        num_island = 0
+                for d_i,d_j in [(0,1),(1,0),(0,-1),(-1,0)]:
+                    next_i,next_j = cur_i+d_i,cur_j+d_j
+                    if 0<=next_i < num_row and 0<=next_j<num_col and grid[next_i][next_j]=='1' and (next_i,next_j) not in visited:
+                        q.append((next_i,next_j))
+                        visited.add((next_i,next_j))
+        num_island=0
         for i in range(num_row):
             for j in range(num_col):
-                if grid[i][j]=='1' and (i,j) not in visited:
+                if (i,j) not in visited and grid[i][j]=='1':
                     bfs(i,j)
                     num_island+=1
         return num_island
+
+        
 
 
             
