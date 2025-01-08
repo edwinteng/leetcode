b2c28e800fb247edf572fef0c492d6ccd766d55c
edwinteng <edwinteng@gmail.com>
2025-01-07 22:42:20 -0500

    Time: 19 ms (48.67%) | Memory: 23.9 MB (10.77%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -4,24 +4,25 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.heap = []
         self.size = k
-        # if k < len(nums)
-        if k < len(nums):
+        if k >= len(nums):
+          for i in range(len(nums)):
+            heapq.heappush(self.heap,nums[i])
+        else:
             for i in range(k):
                 heapq.heappush(self.heap,nums[i])
             for i in range(k,len(nums)):
                 if nums[i]>self.heap[0]:
                     heapq.heappop(self.heap)
                     heapq.heappush(self.heap,nums[i])
-        else:
-            for i in range(len(nums)):
-                heapq.heappush(self.heap,nums[i])            
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap,val)
-        if len(self.heap)>self.size:
-            heapq.heappop(self.heap)
+        if self.size ==len(self.heap):
+            if val > self.heap[0]:
+                heapq.heappop(self.heap)
+                heapq.heappush(self.heap,val)
+        else:
+            heapq.heappush(self.heap,val)
         return self.heap[0]
-
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
