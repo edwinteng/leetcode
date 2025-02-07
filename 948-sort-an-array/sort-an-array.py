66f2eeccb91a06bd051048f8edad9b5514ef5e57
edwinteng <edwinteng@gmail.com>
2025-02-07 00:18:28 -0500

    Time: 587 ms (67.99%) | Memory: 26.6 MB (21.61%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,30 +1,10 @@
 from random import randint
-class Solution(object):
-    def sortArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        def quicksort(left,right):
-            if left >= right:
-                return
-            pivot = nums[randint(left,right)]
-            less_ptr,right_ptr,current=left-1,right+1,left
-            while current < right_ptr:
-                if nums[current]<pivot:
-                    less_ptr+=1
-                    nums[current],nums[less_ptr]=nums[less_ptr],nums[current]
-                    current+=1
-                elif nums[current]>pivot:
-                    right_ptr-=1
-                    nums[current],nums[right_ptr]=nums[right_ptr],nums[current]
-                    #current+=1
-                else:
-                    current+=1
-            quicksort(left,less_ptr)
-            quicksort(right_ptr,right)
-        quicksort(0,len(nums)-1)
-        return nums
-
-
-
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums)<=1:
+            return nums
+        pivot = nums[randint(0,len(nums)-1)]
+        left = [n for n in nums if n < pivot]
+        equal = [n for n in nums if n==pivot]
+        right = [n for n in nums if n>pivot]
+        return self.sortArray(left)+equal+self.sortArray(right)
