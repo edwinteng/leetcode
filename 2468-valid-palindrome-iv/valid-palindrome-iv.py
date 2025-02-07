7d521a95d5e60e88dc31187b73bd1301e6305d50
edwinteng <edwinteng@gmail.com>
2025-02-06 23:32:10 -0500

    Time: 11 ms (23.48%) | Memory: 18.1 MB (16.36%) - LeetSync

--- 2468-valid-palindrome-iv/valid-palindrome-iv.py
+++ 2468-valid-palindrome-iv/valid-palindrome-iv.py
@@ -1,17 +1,12 @@
 class Solution:
     def makePalindrome(self, s: str) -> bool:
-
+        
         start,end = 0,len(s)-1
         cnt = 0
         while start < end:
-            if s[start]==s[end]:
-                start+=1
-                end-=1
-            else:
-                if cnt<2:
-                    cnt+=1
-                    start+=1
-                    end-=1
-                else:
-                    return False
-        return True
+            if s[start]!=s[end]:
+                cnt+=1
+            start+=1
+            end-=1
+
+        return cnt<=2
