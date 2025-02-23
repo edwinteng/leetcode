2cab1c0eea67e2952a03d0bcb7fd59bc1fb3d382
edwinteng <edwinteng@gmail.com>
2025-02-22 19:35:36 -0500

    Time: 1243 ms (14.79%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -3,4 +3,3 @@ SELECT activity_date as day, count(distinct user_id) as active_users
 FROM activity
 WHERE activity_date between  '2019-07-27'-interval '29' day and '2019-07-27' 
 GROUP BY activity_date
-HAVING count(distinct user_id) >0
